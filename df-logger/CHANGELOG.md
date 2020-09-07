599bb5c24d3a5804b0a323f80b4133578b23cd75
Andrew Fryer <flamewave000@live.com>
2020-09-07 12:15:06 -0400

    DF User Logger Release 1.1

---
+++ df-logger/CHANGELOG.md
@@ -0,0 +1,2 @@
+# Release 1.1
+- Changed Auto-Delete setting to a 1-30 slider
--- df-logger/df-logger.js
+++ df-logger/df-logger.js
@@ -2,29 +2,33 @@ import DFLogger from './DFLogger.js';
 
 Hooks.once('init', function () {
 	game.settings.register(DFLogger.MODULE, DFLogger.SETTING_GM_ONLY, {
-		name: game.i18n.localize("DRAGON_FLAGON.Settings_GmOnly_Title"),
-		hint: game.i18n.localize("DRAGON_FLAGON.Settings_GmOnly_Hint"),
+		name: "DRAGON_FLAGON.Settings_GmOnly_Title",
+		hint: "DRAGON_FLAGON.Settings_GmOnly_Hint",
 		scope: "world",
 		config: true,
 		type: Boolean,
 		default: false,
 	});
 	game.settings.register(DFLogger.MODULE, DFLogger.SETTING_NOT_ME, {
-		name: game.i18n.localize("DRAGON_FLAGON.Settings_NotMe_Title"),
-		hint: game.i18n.localize("DRAGON_FLAGON.Settings_NotMe_Hint"),
+		name: "DRAGON_FLAGON.Settings_NotMe_Title",
+		hint: "DRAGON_FLAGON.Settings_NotMe_Hint",
 		scope: "client",
 		config: true,
 		type: Boolean,
 		default: false,
 	});
 	game.settings.register(DFLogger.MODULE, DFLogger.SETTING_DELAY, {
-		name: game.i18n.localize("DRAGON_FLAGON.Settings_Delay_Title"),
-		hint: game.i18n.localize("DRAGON_FLAGON.Settings_Delay_Hint"),
+		name: "DRAGON_FLAGON.Settings_Delay_Title",
+		hint: "DRAGON_FLAGON.Settings_Delay_Hint",
 		scope: "client",
 		config: true,
 		type: Number,
 		default: 15,
-        min: 5,
+		range: {
+			min: 1,
+			max: 30,
+			step: 1
+		},
 		onChange: value => {
 			if (value < 5)
 				game.settings.put(DFLogger.MODULE, DFLogger.SETTING_DELAY, 5);
