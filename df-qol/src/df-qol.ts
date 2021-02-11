33e9f55b02b5053e64dada4edc4f973657fb09a5
Andrew Fryer <flamewave000@live.com>
2021-02-11 12:57:01 -0500

    renamed quick-roll to qol, add autofocus feature

---
+++ df-qol/src/df-qol.ts
@@ -0,0 +1,63 @@
+
+const MODULE_NAME = 'df-qol';
+
+function apply(shouldApply: Boolean, hookName: string, func: Hooks.General) {
+	if (shouldApply) Hooks.on(hookName, func);
+	else Hooks.off(hookName, func);
+}
+
+async function requestReload() {
+	if (await Dialog.confirm({
+		title: game.i18n.localize("DRAGON_FLAGON_QOL.ReloadGameTitle"),
+		content: game.i18n.localize("DRAGON_FLAGON_QOL.ReloadGameContent"),
+		defaultYes: true
+	} as any) as any as Boolean) {
+		window.location.reload();
+	}
+}
+
+Hooks.once('init', function () {
+	game.settings.register(MODULE_NAME, 'quick-roll', {
+		name: 'DRAGON_FLAGON_QOL.QuickRollTitle',
+		hint: 'DRAGON_FLAGON_QOL.QuickRollHint',
+		scope: 'world',
+		type: Boolean,
+		default: true,
+		config:true,
+		onChange: async () => await requestReload()
+	});
+	apply(game.settings.get(MODULE_NAME, 'quick-roll'), 'getRollTableDirectoryEntryContext', DF_QUICK_ROLL);
+
+	game.settings.register(MODULE_NAME, 'auto-focus', {
+		name: 'DRAGON_FLAGON_QOL.AutoFocusTitle',
+		hint: 'DRAGON_FLAGON_QOL.AutoFocusHint',
+		scope: 'world',
+		type: Boolean,
+		default: true,
+		config:true,
+		onChange: newValue => {
+			apply(newValue, 'renderDialog', DF_AUTO_FOCUS)
+			apply(newValue, 'renderFolderConfig', DF_AUTO_FOCUS)
+		}
+	});
+	apply(game.settings.get(MODULE_NAME, 'auto-focus'), 'renderDialog', DF_AUTO_FOCUS);
+	apply(game.settings.get(MODULE_NAME, 'auto-focus'), 'renderFolderConfig', DF_AUTO_FOCUS);
+});
+
+
+function DF_QUICK_ROLL(_html: any, entryOptions: any) {
+	entryOptions.unshift({
+		name: "DRAGON_FLAGON_QOL.QuickRollMenuItem",
+		icon: '<i class="fas fa-dice-d20"></i>',
+		condition: () => true,
+		callback: async (header: any) => {
+			const table = game.tables.get(header.data('entityId'));
+			const roll = table.roll();
+			await table.draw({ roll: roll } as any);
+		}
+	});
+}
+
+function DF_AUTO_FOCUS(_app: any, html: JQuery, _data: any) {
+	html.find('input[type="text"]')[0].focus();
+}
