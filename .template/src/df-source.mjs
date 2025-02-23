78b2ac510971944c489a4a5f07cedfd8fea8ed9f
Andrew Fryer <3588046+flamewave000@users.noreply.github.com>
2025-02-23 10:42:56 -0500

    Updated template folder for v12

---
+++ .template/src/df-source.mjs
@@ -0,0 +1,5 @@
+/// <reference path="../../fvtt-scripts/foundry.js" />
+/// <reference path="../../fvtt-scripts/foundry-esm.js" />
+
+Hooks.once('init', function() {
+});
--- .template/src/df-source.ts
+++
@@ -1,3 +0,0 @@
-
-Hooks.once('init', function() {
-});
