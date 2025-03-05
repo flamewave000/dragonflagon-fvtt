ce984639d83a862c44c741785ee87e31a2aef338
Andrew Fryer <3588046+flamewave000@users.noreply.github.com>
2025-03-05 16:48:20 -0500

    templates: Migrated angle-snaps

--- df-templates/src/df-templates.mjs
+++ df-templates/src/df-templates.mjs
@@ -2,7 +2,7 @@
 /// <reference path="../../common/foundry.d.ts" />
 /// <reference path="./types.d.ts" />
 import SETTINGS from "../common/Settings.mjs";
-// import AngleSnaps from "./AngleSnaps.mjs";
+import AngleSnaps from "./AngleSnaps.mjs";
 import SnapIntersect from "./SnapIntersect.mjs";
 import SquareTemplate from "./SquareTemplate.mjs";
 import TemplateTargeting from "./TemplateTargeting.mjs";
@@ -12,7 +12,7 @@ SETTINGS.init('df-templates');
 Hooks.once('init', function () {
 	TemplateTargeting.init();
 	SnapIntersect.init();
-	// AngleSnaps.init();
+	AngleSnaps.init();
 	SquareTemplate.init();
 
 	// DEBUG SETTINGS
@@ -35,15 +35,15 @@ Hooks.once('ready', function () {
 	}
 	TemplateTargeting.ready();
 	SnapIntersect.ready();
-	// AngleSnaps.ready();
+	AngleSnaps.ready();
 
 	if (game.dnd5e) {
 		libWrapper.register(SETTINGS.MOD_NAME, 'game.dnd5e.canvas.AbilityTemplate.prototype.activatePreviewListeners',
 			function (/**@type {(il: any) => any}*/ wrapper, /**@type {CanvasLayer}*/ initialLayer) {
 				this._onMovePlacement_ORIG = this._onMovePlacement;
 				this._onMovePlacement = SnapIntersect.handleDnD5eAbilityTemplate.bind(this);
-				// this._onRotatePlacement_ORIG = this._onRotatePlacement;
-				// this._onRotatePlacement = AngleSnaps.handleDnD5eAbilityTemplate.bind(this);
+				this._onRotatePlacement_ORIG = this._onRotatePlacement;
+				this._onRotatePlacement = AngleSnaps.handleDnD5eAbilityTemplate.bind(this);
 				return wrapper(initialLayer);
 			}, 'WRAPPER');
 	}
